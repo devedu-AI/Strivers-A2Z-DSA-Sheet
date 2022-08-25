2db52357ffefc757105cde3c7a696a8e13a67817
Amit Maity <maityamit308@gmail.com>
2022-08-25 22:13:06 +0530

    Create LCM And GCD.cpp

---
+++ 001 Learn the basics/Basics Math/LCM And GCD.cpp
@@ -0,0 +1,38 @@
+//{ Driver Code Starts
+#include <bits/stdc++.h>
+using namespace std;
+
+// } Driver Code Ends
+class Solution {
+  public:
+  int gcd(int a,int b)
+  {
+      if(b==0)
+      return a;
+      return gcd(b,a%b);
+  }
+    vector<long long> lcmAndGcd(long long A , long long B) {
+        // code here
+        vector<long long>v;
+	v.push_back((A*B)/gcd(A,B));
+	v.push_back(gcd(A,B));
+	return v;
+    }
+};
+
+//{ Driver Code Starts.
+int main() {
+    int t;
+    cin >> t;
+    while (t--) {
+        long long A,B;
+        
+        cin>>A>>B;
+
+        Solution ob;
+        vector<long long> ans = ob.lcmAndGcd(A,B);
+        cout<<ans[0]<<" "<<ans[1]<<endl;
+    }
+    return 0;
+}
+// } Driver Code Ends
